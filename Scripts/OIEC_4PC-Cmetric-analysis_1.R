15622112e4b73c6c0e07de13787ea7ecfdcaaf94
Travis Park <t.park@nhm.ac.uk>
2018-10-18 11:25:24 +0100

    Updates to previous scripts and addition of script examining PC variation.

---
+++ Scripts/OIEC_4PC-Cmetric-analysis_1.R
@@ -0,0 +1,52 @@
+
+
+library(convevol)
+
+OIEC_Cmetricmatrix <- as.matrix(array(OIEC_df[, 1:4])) # The numbers here are what PCs you select for the analysis
+
+convtips1 <- c(
+  "Cephalorhynchus_commersonii",
+  "Cephalorhynchus_heavisidii",
+  "Delphinapterus_leucas",
+  "Delphinus_delphis",
+  "Feresa_attenuata",
+  "Grampus_griseus",
+  "Inia_geoffrensis",
+  "Lagenodelphis_hosei",
+  "Lagenorhynchus_acutus",
+  "Lagenorhynchus_albirostris",
+  "Lagenorhynchus_australis",
+  "Lagenorhynchus_cruciger",
+  "Lagenorhynchus_obscurus",
+  "Lipotes_vexillifer",
+  "Monodon_monoceros",
+  "Neophocaena_phocaenoides",
+  "Orcaella_brevirostris",
+  "Orcinus_orca",
+  "Peponocephala_electra",
+  "Phocoena_dioptrica",
+  "Phocoena_phocoena",
+  "Phocoenoides_dalli",
+  "Platanista_gangetica",
+  "Pontoporia_blainvillei",
+  "Pseudorca_crassidens",
+  "Sotalia_fluviatilis",
+  "Sotalia_guianensis",
+  "Sousa_chinensis",
+  "Stenella_attenuata",
+  "Stenella_coeruleoalba",
+  "Stenella_longirostris",
+  "Steno_bredanensis",
+  "Tursiops_aduncus",
+  "Tursiops_truncatus"
+)
+
+ConvReg1 <- convrat(phyl = cleanwhaletree,
+                    phendata = OIEC_Cmetricmatrix,
+                    convtips = convtips1)
+ConvReg1
+
+ConvReg1_pval <- convratsig(phyl = cleanwhaletree,
+                            phendata = OIEC_Cmetricmatrix,
+                            convtips = c
+ConvReg1_pval
---
+++ Scripts/OIEC_examining-PC-variation_1.R
@@ -0,0 +1,60 @@
+### looking at PC variation using wireframe and mesh-warping methods
+### created 17/10/2018 by Travis Park
+
+library(geomorph)
+library(Morpho)
+
+## need to have alrfeady run the GPA and PCA for the OIEC analysis
+
+# find the mean species (it's Orcinus orca - number 28)
+meanspec <- findMeanSpec(OIEC_gpa$coords)
+
+# assign it to an object to act as a reference point to compare to the PC extremes
+ref <- OIEC_gpa$coords[, , meanspec]
+
+## need to create a 3d mesh of the mean cochlea shape
+
+# find the mean cochlear shape using the GPA coords
+meanshape <- mshape(OIEC_gpa$coords)
+
+# Load in the orca ply file to act as a reference point to warp into the mean mesh shape
+refmesh <- read.ply("Landmarks/L371-cochlea-dataset/RDC_L371-cochlea_pts/ECHO_L371-cochlea_ply/Orcinus-orca_NHMUK1927.28_3.ply")
+
+# load in the orca ply coords
+refcoords <- read.pts("Landmarks/L371-cochlea-dataset/RDC_L371-cochlea_pts/Orcinus-orca_NHMUK1927.28_3.pts")
+
+# warp the orca mesh into the mean mesh
+meanmesh <- warpRefMesh(mesh = refmesh, mesh.coord = refcoords, ref = meanshape)
+
+
+## look at PC variation
+
+# create wireframes of PC extremes
+plotRefToTarget(ref, OIEC_pca$pc.shapes$PC1min)
+plotRefToTarget(ref, OIEC_pca$pc.shapes$PC1max)
+plotRefToTarget(ref, OIEC_pca$pc.shapes$PC2min)
+plotRefToTarget(ref, OIEC_pca$pc.shapes$PC2max)
+
+# create vectors showing of PC extremes
+plotRefToTarget(ref, OIEC_pca$pc.shapes$PC1min, method = "vector")
+plotRefToTarget(ref, OIEC_pca$pc.shapes$PC1max, method = "vector")
+plotRefToTarget(ref, OIEC_pca$pc.shapes$PC2min, method = "vector")
+plotRefToTarget(ref, OIEC_pca$pc.shapes$PC2max, method = "vector")
+
+
+
+# create points showing difference between mean species and PC extremes
+plotRefToTarget(ref, OIEC_pca$pc.shapes$PC1min, method = "points")
+plotRefToTarget(ref, OIEC_pca$pc.shapes$PC1max, method = "points")
+plotRefToTarget(ref, OIEC_pca$pc.shapes$PC2min, method = "points")
+plotRefToTarget(ref, OIEC_pca$pc.shapes$PC2max, method = "points")
+
+# create 3d mesh of the PC extremes based on the orca refmesh
+plotRefToTarget(ref, OIEC_pca$pc.shapes$PC1min, mesh = refmesh, method = "surface")
+plotRefToTarget(ref, OIEC_pca$pc.shapes$PC1max, mesh = refmesh, method = "surface")
+plotRefToTarget(ref, OIEC_pca$pc.shapes$PC2min, mesh = refmesh, method = "surface")
+plotRefToTarget(ref, OIEC_pca$pc.shapes$PC2max, mesh = refmesh, method = "surface")
+
+
+
+
